ab3a2c8f2386fd2c4b46da51aa48d5d75001325e
Mark Munkacsi <markmunkacsi@protonmail.com>
2021-08-06 08:05:22 +0400

    Fix tests

--- test/LocalDataStorage.test.js
+++ test/LocalDataStorage.test.js
@@ -1,14 +1,20 @@
-import LocalDataStorage from '../src/LocalDataStorage';
-import LocalDataStorageItem from '../src/LocalDataStorageItem';
+/**
+ * @jest-environment jsdom
+ */
+
+import LocalDataStorage from "../src/LocalDataStorage";
+import LocalDataStorageItem from "../src/LocalDataStorageItem";
 
 const originalLocalStorage = global.localStorage;
-const disableLocalStorage = () => Object
-  .defineProperty(global, 'localStorage', {
-    value: undefined, writable: true,
+const disableLocalStorage = () =>
+  Object.defineProperty(global, "localStorage", {
+    value: undefined,
+    writable: true,
   });
-const enableLocalStorage = () => Object
-  .defineProperty(global, 'localStorage', {
-    value: originalLocalStorage, writable: true,
+const enableLocalStorage = () =>
+  Object.defineProperty(global, "localStorage", {
+    value: originalLocalStorage,
+    writable: true,
   });
 
 afterEach(() => {
@@ -16,16 +22,16 @@ afterEach(() => {
   global.localStorage.clear();
 });
 
-describe('constructor', () => {
-  test('it should be a singleton', () => {
+describe("constructor", () => {
+  test("it should be a singleton", () => {
     const localDataStorage = new LocalDataStorage();
-    const uniquePropertyValue = '🦄';
+    const uniquePropertyValue = "🦄";
     localDataStorage.uniqueProperty = uniquePropertyValue;
     const anotherLocalDataStorage = new LocalDataStorage();
     expect(anotherLocalDataStorage.uniqueProperty).toEqual(uniquePropertyValue);
   });
 
-  test('it\'s storage must be null if localStorage isn\'t available', () => {
+  test("it's storage must be null if localStorage isn't available", () => {
     disableLocalStorage();
     const localDataStorage = new LocalDataStorage();
     expect(localDataStorage.storage).toBeNull();
@@ -33,8 +39,8 @@ describe('constructor', () => {
   });
 });
 
-describe('getter isAvailable', () => {
-  test('works', () => {
+describe("getter isAvailable", () => {
+  test("works", () => {
     const localDataStorage = new LocalDataStorage();
     localDataStorage.storage = null;
     expect(localDataStorage.isAvailable).toBeFalsy();
@@ -43,19 +49,19 @@ describe('getter isAvailable', () => {
   });
 });
 
-describe('method getItem', () => {
-  test('if something goes wrong throws an exception', () => {
+describe("method getItem", () => {
+  test("if something goes wrong throws an exception", () => {
     disableLocalStorage();
     const localDataStorage = new LocalDataStorage();
-    expect(() => localDataStorage.getItem('🔑')).toThrow(TypeError);
+    expect(() => localDataStorage.getItem("🔑")).toThrow(TypeError);
     enableLocalStorage();
   });
 
-  test('should handle foreign items', () => {
-    const itemKey = '🔑';
-    const itemKey2 = '🗝';
+  test("should handle foreign items", () => {
+    const itemKey = "🔑";
+    const itemKey2 = "🗝";
     const parseableValue = '{"📦":["🐷","🐮","🐰"]}';
-    const justAStringValue = '¯\\_(ツ)_/¯';
+    const justAStringValue = "¯\\_(ツ)_/¯";
     global.localStorage.setItem(itemKey, parseableValue);
     global.localStorage.setItem(itemKey2, justAStringValue);
     const localDataStorage = new LocalDataStorage();
@@ -65,14 +71,14 @@ describe('method getItem', () => {
     expect(returnedItem2.value).toEqual(justAStringValue);
   });
 
-  test('should return null if item doesn\'t exist', () => {
+  test("should return null if item doesn't exist", () => {
     const localDataStorage = new LocalDataStorage();
-    expect(localDataStorage.getItem('macska')).toBeNull();
+    expect(localDataStorage.getItem("macska")).toBeNull();
   });
 
-  test('should return the item correctly', () => {
-    const itemKey = '🔑';
-    const itemValue = '🐈';
+  test("should return the item correctly", () => {
+    const itemKey = "🔑";
+    const itemValue = "🐈";
     global.localStorage.setItem(itemKey, JSON.stringify({ value: itemValue }));
     const localDataStorage = new LocalDataStorage();
     const returnedItem = localDataStorage.getItem(itemKey);
@@ -81,39 +87,44 @@ describe('method getItem', () => {
   });
 });
 
-describe('method setItem', () => {
-  test('if something goes wrong throws an exception', () => {
+describe("method setItem", () => {
+  test("if something goes wrong throws an exception", () => {
     disableLocalStorage();
     const localDataStorage = new LocalDataStorage();
-    expect(() => localDataStorage.setItem('🔑', '🐈')).toThrow(TypeError);
+    expect(() => localDataStorage.setItem("🔑", "🐈")).toThrow(TypeError);
     enableLocalStorage();
   });
 
-  test('should return false when item already exists and it isn\'t forced', () => {
-    const itemKey = '🔑';
-    const itemValue = '🐈';
+  test("should return false when item already exists and it isn't forced", () => {
+    const itemKey = "🔑";
+    const itemValue = "🐈";
     global.localStorage.setItem(itemKey, JSON.stringify({ value: itemValue }));
     const localDataStorage = new LocalDataStorage();
-    const returnedBool = localDataStorage.setItem(itemKey, '🐱');
+    const returnedBool = localDataStorage.setItem(itemKey, "🐱");
     expect(returnedBool).toBeFalsy();
     const storedItem = JSON.parse(global.localStorage.getItem(itemKey));
     expect(storedItem.value).toEqual(itemValue);
   });
 
-  test('should return true when saving was successful', () => {
-    const itemKey = '🔑';
-    const newItemValue = '🐱';
-    global.localStorage.setItem(itemKey, JSON.stringify({ value: '🐈' }));
+  test("should return true when saving was successful", () => {
+    const itemKey = "🔑";
+    const newItemValue = "🐱";
+    global.localStorage.setItem(itemKey, JSON.stringify({ value: "🐈" }));
     const localDataStorage = new LocalDataStorage();
-    const returnedBool = localDataStorage.setItem(itemKey, newItemValue, null, true);
+    const returnedBool = localDataStorage.setItem(
+      itemKey,
+      newItemValue,
+      null,
+      true
+    );
     expect(returnedBool).toBeTruthy();
     const storedItem = JSON.parse(global.localStorage.getItem(itemKey));
     expect(storedItem.value).toEqual(newItemValue);
   });
 
-  test('should handle input parameters correctly', () => {
-    const itemKey = '🔑';
-    const itemValue = '🐈';
+  test("should handle input parameters correctly", () => {
+    const itemKey = "🔑";
+    const itemValue = "🐈";
     const expiry = Date.now();
     const localDataStorage = new LocalDataStorage();
     const returnedBool = localDataStorage.setItem(itemKey, itemValue, expiry);
@@ -124,23 +135,23 @@ describe('method setItem', () => {
   });
 });
 
-describe('method removeItem', () => {
-  test('if something goes wrong throws an exception', () => {
+describe("method removeItem", () => {
+  test("if something goes wrong throws an exception", () => {
     disableLocalStorage();
     const localDataStorage = new LocalDataStorage();
-    expect(() => localDataStorage.removeItem('🔑')).toThrow(TypeError);
+    expect(() => localDataStorage.removeItem("🔑")).toThrow(TypeError);
     enableLocalStorage();
   });
 
-  test('should return false when the item doesn\'t exist', () => {
+  test("should return false when the item doesn't exist", () => {
     const localDataStorage = new LocalDataStorage();
-    const returnedBool = localDataStorage.removeItem('🔑');
+    const returnedBool = localDataStorage.removeItem("🔑");
     expect(returnedBool).toBeFalsy();
   });
 
-  test('should return true and remove item when it exists', () => {
-    const itemKey = '🔑';
-    global.localStorage.setItem(itemKey, JSON.stringify({ value: '🐈' }));
+  test("should return true and remove item when it exists", () => {
+    const itemKey = "🔑";
+    global.localStorage.setItem(itemKey, JSON.stringify({ value: "🐈" }));
     const localDataStorage = new LocalDataStorage();
     const returnedBool = localDataStorage.removeItem(itemKey);
     expect(returnedBool).toBeTruthy();
@@ -149,16 +160,16 @@ describe('method removeItem', () => {
   });
 });
 
-describe('method clear', () => {
-  test('if something goes wrong throws an exception', () => {
+describe("method clear", () => {
+  test("if something goes wrong throws an exception", () => {
     disableLocalStorage();
     const localDataStorage = new LocalDataStorage();
     expect(() => localDataStorage.clear()).toThrow(TypeError);
     enableLocalStorage();
   });
 
-  test('should return true and wipe the storage out', () => {
-    global.localStorage.setItem('🔑', JSON.stringify({ value: '🐈' }));
+  test("should return true and wipe the storage out", () => {
+    global.localStorage.setItem("🔑", JSON.stringify({ value: "🐈" }));
     const localDataStorage = new LocalDataStorage();
     const returnedBool = localDataStorage.clear();
     expect(returnedBool).toBeTruthy();
@@ -166,55 +177,70 @@ describe('method clear', () => {
   });
 });
 
-describe('method itemExists', () => {
-  test('should return true if the item exists', () => {
-    const itemKey = '🔑';
-    global.localStorage.setItem(itemKey, JSON.stringify({ value: '🐈' }));
+describe("method itemExists", () => {
+  test("should return true if the item exists", () => {
+    const itemKey = "🔑";
+    global.localStorage.setItem(itemKey, JSON.stringify({ value: "🐈" }));
     const localDataStorage = new LocalDataStorage();
     const returnedBool = localDataStorage.itemExists(itemKey);
     expect(returnedBool).toBeTruthy();
   });
 
-  test('should return false if the item doesn\'t exist', () => {
+  test("should return false if the item doesn't exist", () => {
     const localDataStorage = new LocalDataStorage();
-    const returnedBool = localDataStorage.itemExists('🔑');
+    const returnedBool = localDataStorage.itemExists("🔑");
     expect(returnedBool).toBeFalsy();
   });
 });
 
-describe('method clearExpiredItems', () => {
-  test('should return false if there was no deletion', () => {
+describe("method clearExpiredItems", () => {
+  test("should return false if there was no deletion", () => {
     const localDataStorage = new LocalDataStorage();
     const returnedBool = localDataStorage.clearExpiredItems();
     expect(returnedBool).toBeFalsy();
   });
 
-  test('should return true if there was one or more deletion', () => {
+  test("should return true if there was one or more deletion", () => {
     const localDataStorage = new LocalDataStorage();
-    global.localStorage.setItem('🔑', JSON.stringify({
-      value: '🐈', expiryDate: 0,
-    }));
+    global.localStorage.setItem(
+      "🔑",
+      JSON.stringify({
+        value: "🐈",
+        expiryDate: 0,
+      })
+    );
     const returnedBool = localDataStorage.clearExpiredItems();
     expect(returnedBool).toBeTruthy();
   });
 
-  test('should remove only validated expired items', () => {
+  test("should remove only validated expired items", () => {
     const oneDayTimestamp = 86400;
-    const notExpiredItemKey = '🔑';
-    const expiredItemKey = '🗝';
-    const foreignItemKey = '⛺️';
-    const nullExpiryItemKey = '👻';
+    const notExpiredItemKey = "🔑";
+    const expiredItemKey = "🗝";
+    const foreignItemKey = "⛺️";
+    const nullExpiryItemKey = "👻";
     const notExpiredTime = Date.now() + oneDayTimestamp;
     const expiredTime = Date.now() - oneDayTimestamp;
-    const valueOfAll = '💩';
-    global.localStorage.setItem(notExpiredItemKey, JSON.stringify({
-      value: valueOfAll, expiryDate: notExpiredTime,
-    }));
-    global.localStorage.setItem(expiredItemKey, JSON.stringify({
-      value: valueOfAll, expiryDate: expiredTime,
-    }));
+    const valueOfAll = "💩";
+    global.localStorage.setItem(
+      notExpiredItemKey,
+      JSON.stringify({
+        value: valueOfAll,
+        expiryDate: notExpiredTime,
+      })
+    );
+    global.localStorage.setItem(
+      expiredItemKey,
+      JSON.stringify({
+        value: valueOfAll,
+        expiryDate: expiredTime,
+      })
+    );
     global.localStorage.setItem(foreignItemKey, valueOfAll);
-    global.localStorage.setItem(nullExpiryItemKey, JSON.stringify({ value: valueOfAll }));
+    global.localStorage.setItem(
+      nullExpiryItemKey,
+      JSON.stringify({ value: valueOfAll })
+    );
     const localDataStorage = new LocalDataStorage();
     const expiredItem = global.localStorage.getItem(expiredItemKey);
     expect(expiredItem).toBeNull();
--- test/validator.test.js
+++ test/validator.test.js
@@ -1,45 +1,49 @@
-import isLocalStorageAvailable from '../src/validator';
+/**
+ * @jest-environment jsdom
+ */
+
+import isLocalStorageAvailable from "../src/validator";
 import {
   emptyLocalStorage,
   notFirefoxLocalStorage,
   firefoxLocalStorage,
   fullLocalStorage,
-} from './localStorage.mock';
+} from "./localStorage.mock";
 
 const originalLocalStorage = global.localStorage;
-const setLocalStorage = mockLocalStorage => Object
-  .defineProperty(global, 'localStorage', {
+const setLocalStorage = (mockLocalStorage) =>
+  Object.defineProperty(global, "localStorage", {
     value: mockLocalStorage,
     writable: true,
   });
-const resetLocalStorage = () => Object
-  .defineProperty(global, 'localStorage', {
+const resetLocalStorage = () =>
+  Object.defineProperty(global, "localStorage", {
     value: originalLocalStorage,
     writable: true,
   });
 
 afterEach(() => resetLocalStorage());
 
-test('should return false if there isn\'t localStorage on window', () => {
+test("should return false if there isn't localStorage on window", () => {
   setLocalStorage(emptyLocalStorage());
   expect(isLocalStorageAvailable()).toBeFalsy();
 });
 
-test('should return false when browser (except Firefox) doesn\'t support localStorage or Safari lies about it in private mode (<iOS11)', () => {
+test("should return false when browser (except Firefox) doesn't support localStorage or Safari lies about it in private mode (<iOS11)", () => {
   setLocalStorage(notFirefoxLocalStorage(global));
   expect(isLocalStorageAvailable()).toBeFalsy();
 });
 
-test('should return false if Firefox doesn\'t support localStorage', () => {
+test("should return false if Firefox doesn't support localStorage", () => {
   setLocalStorage(firefoxLocalStorage());
   expect(isLocalStorageAvailable()).toBeFalsy();
 });
 
-test('should return true if the localStorage is full but it\'s available', () => {
+test("should return true if the localStorage is full but it's available", () => {
   setLocalStorage(fullLocalStorage());
   expect(isLocalStorageAvailable()).toBeTruthy();
 });
 
-test('should return true if the localStorage is fully working', () => {
+test("should return true if the localStorage is fully working", () => {
   expect(isLocalStorageAvailable()).toBeTruthy();
 });
