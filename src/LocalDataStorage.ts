f7bd060c298716afc6776cb2834c6c61b5209771
Mark Munkacsi <markmunkacsi@protonmail.com>
2022-03-25 02:52:19 +0400

    fix webpack error

--- src/LocalDataStorage.ts
+++ src/LocalDataStorage.ts
@@ -1,7 +1,24 @@
-import { isLocalStorageAvailable } from "./validator";
-import type LocalDataStorageItem from "./LocalDataStorageItem";
+import type LocalDataStorageItem from "./LocalDataStorageItem.js";
 
-const isAvailable = (): boolean => isLocalStorageAvailable();
+const isAvailable = (): boolean => {
+	const storage: Storage = window.localStorage;
+	try {
+		const testItem: string = "__storage_test__";
+		storage.setItem(testItem, testItem);
+		storage.removeItem(testItem);
+		return true;
+	} catch (e) {
+		return (
+			e instanceof DOMException &&
+			(e.code === 22 ||
+				e.code === 1014 ||
+				e.name === "QuotaExceededError" ||
+				e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+			storage &&
+			storage.length !== 0
+		);
+	}
+};
 
 const itemExists = (key: string): boolean => getItem(key) !== null;
 
